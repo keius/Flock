8ea54fe81a6c13a88ece62b68b28ec84c063c29d
Greg <waterparkmelon@gmail.com>
2017-07-28 17:21:07 -0700

    heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,23 +1,22 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
-  const createLogger = require('redux-logger');
-  middlewares.push(createLogger());
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   // must use 'require' (import only allowed at top of file)
+//   const createLogger = require('redux-logger');
+//   middlewares.push(createLogger());
+// }
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    composeEnhancers(applyMiddleware(thunk, logger))
+    composeEnhancers(applyMiddleware(thunk))
   )
 );
 
