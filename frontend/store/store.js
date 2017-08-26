465c7ed978f9bc952113bfa2194bacb9d015630d
Greg <waterparkmelon@gmail.com>
2017-08-25 23:47:59 -0700

    fix minify

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -6,11 +6,11 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middlewares = [thunk];
 
-// if (process.env.NODE_ENV !== 'production') {
-//   // must use 'require' (import only allowed at top of file)
-//   const createLogger = require('redux-logger');
-//   middlewares.push(createLogger());
-// }
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const createLogger = require('redux-logger').createLogger;
+  middlewares.push(createLogger());
+}
 
 const configureStore = (preloadedState = {}) => (
   createStore(
